0cb95f7011b9c7dd0a56c6baa1f6160bef1c7143
Jesus Calderin <jesuscalderin91@gmail.com>
2020-02-09 19:26:31 -0400

    Updates

---
+++ precache-manifest.be4e60d94256187f6e55105a40b3744f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3c3311bf5eaf4e661350352ef9a665",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "1de681affa538fd0fc5f",
+    "url": "/react_app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1ff159e296ac45db17fd",
+    "url": "/react_app/static/css/main.41973bd3.chunk.css"
+  },
+  {
+    "revision": "1de681affa538fd0fc5f",
+    "url": "/react_app/static/js/2.36d062cf.chunk.js"
+  },
+  {
+    "revision": "1ff159e296ac45db17fd",
+    "url": "/react_app/static/js/main.cace3eb2.chunk.js"
+  },
+  {
+    "revision": "dd7a70cdeda87e112e79",
+    "url": "/react_app/static/js/runtime~main.cba007e3.js"
+  }
+]);
